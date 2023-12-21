2eae52d316274d4d620ec2e4c9515dd8ea3d566d
Apple <apple@MacBook-Pro-Apple.local>
2023-12-21 12:39:43 +0300

    test

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,109 +1,108 @@
 <template>
-    <main class="content">
-    <form action="#" method="post">
+  <main class="content">
+  <form action="#" method="post">
 
-      <div class="content__wrapper">
-        <h1 class="title title--big">Конструктор пиццы</h1>
+    <div class="content__wrapper">
+      <h1 class="title title--big">Конструктор пиццы</h1>
 
-        <div class="content__dough">
+      <div class="content__dough">
 
-          <div class="sheet">
-            <h2 class="title title--small sheet__title">Выберите тесто</h2>
+        <div class="sheet">
+          <h2 class="title title--small sheet__title">Выберите тесто</h2>
 
-            <div v-for="item in dough" class="sheet__content dough">
-              <label class="`dough__input dough__input--${light}`">
-                <input type="radio" name="dought" value="`${light}`" class="visually-hidden" checked>
-                <b> {{ item.name }} </b>
-                <span>Из твердых сортов пшеницы</span>
-              </label>
-            </div>
-
+          <div v-for="item in dough" class="sheet__content dough">
+            <label class="`dough__input dough__input--${light}`">
+              <input type="radio" name="dought" value="`${light}`" class="visually-hidden" checked>
+              <b> {{ item.name }} </b>
+              <span>Из твердых сортов пшеницы</span>
+            </label>
           </div>
 
-        </div>
-
-        <div class="content__diameter">
-          <div class="sheet">
-            <h2 class="title title--small sheet__title">Выберите размер</h2>
-
-            <div v-for="size in sizes" class="sheet__content diameter">
-              <label class="`diameter__input diameter__input--${small}`">
-                <input type="radio" name="diameter" value="`${small}`" class="visually-hidden" checked>
-                <span> {{ size.name }} </span>
-              </label>
-
-            </div>
-          </div>
-        </div>
-
-        <div class="content__ingredients">
-          <div class="sheet">
-            <h2 class="title title--small sheet__title">Выберите ингредиенты</h2>
-
-            <div class="sheet__content ingredients">
-
-              <div v-for="sauce in sauces" class="ingredients__sauce">
-                <p>Основной соус:</p>
-
-                <label class="radio ingredients__input">
-                  <input type="radio" name="sauce" value="`${tomato}`" checked>
-                  <span> {{ sauce.name }} </span>
-                </label>
- 
-              </div>
-
-              <div class="ingredients__filling">
-                <p>Начинка:</p>
-
-                <ul class="ingredients__list">
-                  <li v-for="ingredient in ingredients" class="ingredients__item">
-                    <span class="`filling filling--${mushrooms}`"> {{ingredient.name}} </span>
-
-                    <div class="counter counter--orange ingredients__counter">
-                      <button type="button" class="counter__button counter__button--minus" disabled>
-                        <span class="visually-hidden">Меньше</span>
-                      </button>
-                      <input type="text" name="counter" class="counter__input" value="0">
-                      <button type="button" class="counter__button counter__button--plus">
-                        <span class="visually-hidden">Больше</span>
-                      </button>
-                    </div>
-                  </li>
-
-                </ul>
-
-              </div>
-
-            </div>
-          </div>
-        </div>
-
-        <div class="content__pizza">
-          <label class="input">
-            <span class="visually-hidden">Название пиццы</span>
-            <input type="text" name="pizza_name" placeholder="Введите название пиццы">
-          </label>
-
-          <div class="content__constructor">
-            <div class="pizza pizza--foundation--big-tomato">
-              <div class="pizza__wrapper">
-                <div class="pizza__filling pizza__filling--ananas"></div>
-                <div class="pizza__filling pizza__filling--bacon"></div>
-                <div class="pizza__filling pizza__filling--cheddar"></div>
-              </div>
-            </div>
-          </div>
-
-          <div class="content__result">
-            <p>Итого: 0 ₽</p>
-            <button type="button" class="button" disabled>Готовьте!</button>
-          </div>
         </div>
 
       </div>
 
-    </form>
-  </main>
+      <div class="content__diameter">
+        <div class="sheet">
+          <h2 class="title title--small sheet__title">Выберите размер</h2>
+
+          <div v-for="size in sizes" class="sheet__content diameter">
+            <label class="`diameter__input diameter__input--${small}`">
+              <input type="radio" name="diameter" value="`${small}`" class="visually-hidden" checked>
+              <span> {{ size.name }} </span>
+            </label>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="content__ingredients">
+        <div class="sheet">
+          <h2 class="title title--small sheet__title">Выберите ингредиенты</h2>
+
+          <div class="sheet__content ingredients">
+
+            <div class="ingredients__sauce">
+              <p>Основной соус:</p>
+
+              <label v-for="sauce in sauces" class="radio ingredients__input">
+                <input type="radio" name="sauce" value="`${tomato}`" checked>
+                <span> {{ sauce.name }} </span>
+              </label>
+            </div>
+
+            <div class="ingredients__filling">
+              <p>Начинка:</p>
+
+              <ul class="ingredients__list">
+                <li v-for="ingredient in ingredients" class="ingredients__item">
+                  <span class="`filling filling--${mushrooms}`"> {{ingredient.name}} </span>
+
+                  <div class="counter counter--orange ingredients__counter">
+                    <button type="button" class="counter__button counter__button--minus" disabled>
+                      <span class="visually-hidden">Меньше</span>
+                    </button>
+                    <input type="text" name="counter" class="counter__input" value="0">
+                    <button type="button" class="counter__button counter__button--plus">
+                      <span class="visually-hidden">Больше</span>
+                    </button>
+                  </div>
+                </li>
+                
+              </ul>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="content__pizza">
+        <label class="input">
+          <span class="visually-hidden">Название пиццы</span>
+          <input type="text" name="pizza_name" placeholder="Введите название пиццы">
+        </label>
+
+        <div class="content__constructor">
+          <div class="pizza pizza--foundation--big-tomato">
+            <div class="pizza__wrapper">
+              <div class="pizza__filling pizza__filling--ananas"></div>
+              <div class="pizza__filling pizza__filling--bacon"></div>
+              <div class="pizza__filling pizza__filling--cheddar"></div>
+            </div>
+          </div>
+        </div>
+
+        <div class="content__result">
+          <p>Итого: 0 ₽</p>
+          <button type="button" class="button" disabled>Готовьте!</button>
+        </div>
+      </div>
+
+    </div>
+
+  </form>
+</main>
 </template>
 
 <script setup>
@@ -113,3 +112,79 @@ import misc from '../mocks/misc.json'
 import sauces from '../mocks/sauces.json'
 import sizes from '../mocks/sizes.json'
 </script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/app.scss";
+.container {
+width: 770px;
+margin: 0 auto;
+}
+.content {
+padding-top: 20px;
+}
+
+.content__wrapper {
+display: flex;
+align-items: flex-start;
+flex-wrap: wrap;
+
+width: 920px;
+margin: 0 auto;
+padding-right: 2.12%;
+padding-bottom: 30px;
+padding-left: 2.12%;
+}
+
+.content__dough {
+width: 527px;
+margin-top: 15px;
+margin-right: auto;
+margin-bottom: 15px;
+}
+
+.content__diameter {
+width: 373px;
+margin-top: 15px;
+margin-bottom: 15px;
+}
+
+.content__ingredients {
+width: 527px;
+margin-top: 15px;
+margin-right: auto;
+margin-bottom: 15px;
+}
+
+.content__pizza {
+width: 373px;
+margin-top: 15px;
+margin-bottom: 15px;
+}
+
+.content__constructor {
+width: 315px;
+margin-top: 25px;
+margin-right: auto;
+margin-left: auto;
+}
+
+.content__result {
+display: flex;
+align-items: center;
+justify-content: center;
+
+margin-top: 25px;
+
+p {
+  @include b-s24-h28;
+
+  margin: 0;
+}
+
+button {
+  margin-left: 12px;
+  padding: 16px 45px;
+}
+}
+
+</style>

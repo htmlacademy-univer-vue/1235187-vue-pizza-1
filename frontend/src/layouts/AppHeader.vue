77c41a4b07ac55137381bda5132a094b1a03fb30
Apple <apple@MacBook-Pro-Apple.local>
2023-12-24 12:44:37 +0300

    добавлены общие компоненты

--- frontend/src/layouts/AppHeader.vue
+++ frontend/src/layouts/AppHeader.vue
@@ -1,22 +1,47 @@
 <template>
-    <header class="header">
-        <div class="header__logo">
-            <a href="index.html" class="logo">
-                <img src="img/logo.svg" alt="V!U!E! Pizza logo" width="90" height="40">
-            </a>
-        </div>
-        <div class="header__cart">
-            <a href="cart.html">0 ₽</a>
-        </div>
-        <div class="header__user">
-            <a href="#" class="header__login"><span>Войти</span></a>
-        </div>
-    </header>
+  <header class="header">
+    <div class="header__logo">
+      <a href="/" class="logo">
+        <img
+          src="@/assets/img/logo.svg"
+          alt="V!U!E! Pizza logo"
+          width="90"
+          height="40"
+        />
+      </a>
+    </div>
+    <div class="header__cart">
+      <a href="/">0 ₽</a>
+    </div>
+    <div class="header__user">
+      <a href="/">
+        <picture>
+          <source
+            type="image/webp"
+            srcset="
+              @/assets/img/users/user5.webp    1x,
+              @/assets/img/users/user5@2x.webp 2x
+            "
+          />
+          <img
+            src="@/assets/img/users/user5.jpg"
+            srcset="@/assets/img/users/user5@2x.jpg"
+            alt="Василий Ложкин"
+            width="32"
+            height="32"
+          />
+        </picture>
+        <span>Василий Ложкин</span>
+      </a>
+      <a href="/" class="header__logout">
+        <span>Выйти</span>
+      </a>
+    </div>
+  </header>
 </template>
 
 <style lang="scss" scoped>
-
-@import "@/assets/scss/app.scss";
+@import "@/assets/scss/ds-system/ds";
 
 .header {
   position: relative;
@@ -44,16 +69,13 @@
 
     display: block;
 
-    padding-top: 21px;
-    padding-right: 15px;
-    padding-bottom: 21px;
-    padding-left: 58px;
+    padding: 21px 15px 21px 58px;
 
     transition: 0.3s;
 
     color: $white;
     background-color: $green-500;
-    background-image: url("../img/cart.svg");
+    background-image: url("@/assets/img/cart.svg");
     background-repeat: no-repeat;
     background-position: 20px center;
     background-size: 29px 27px;
@@ -79,10 +101,7 @@
   a {
     display: block;
 
-    padding-top: 14px;
-    padding-right: 20px;
-    padding-bottom: 14px;
-    padding-left: 20px;
+    padding: 14px 20px;
 
     transition: 0.3s;
 
@@ -132,14 +151,13 @@
     height: 32px;
     margin-right: 8px;
 
-    content: '';
+    content: "";
     vertical-align: middle;
 
-    background: url(../img/login.svg) no-repeat center;
+    background: url("@/assets/img/login.svg") no-repeat center;
     background-size: auto 50%;
   }
 }
-
 
 .header__login {
   &::after {
@@ -149,12 +167,11 @@
     height: 32px;
     margin-left: 8px;
 
-    content: '';
+    content: "";
     vertical-align: middle;
 
-    background: url(../img/login.svg) no-repeat center;
+    background: url("@/assets/img/login.svg") no-repeat center;
     background-size: auto 50%;
   }
 }
 </style>
-
